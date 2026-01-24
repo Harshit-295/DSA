05b5ae2b2e9aac64e1568f6835f63275a64bf16b
Harshit Bhardwaj <bhardwajharsh1984@gmail.com>
2026-01-24 07:29:51 +0530

    Time: 670 ms (61.19%) | Memory: 48.8 MB (60.45%) - LeetSync

--- 2108-minimize-the-difference-between-target-and-chosen-elements/minimize-the-difference-between-target-and-chosen-elements.java
+++ 2108-minimize-the-difference-between-target-and-chosen-elements/minimize-the-difference-between-target-and-chosen-elements.java
@@ -1,25 +1,30 @@
 class Solution {
-
-    int helper(int[][] mat, int target, int i, int sum, Integer[][] dp) {
-        if (i == mat.length) {
-            return Math.abs(sum - target);
-        }
-        if (dp[i][sum] != null) {
-            return dp[i][sum];
-        }
-        int minDiff = Integer.MAX_VALUE;
-        for (int j = 0; j < mat[0].length; j++) {
-            int diff = helper(mat, target, i + 1, sum + mat[i][j], dp);
-            minDiff = Math.min(minDiff, diff);
-        }
-
-        dp[i][sum] = minDiff;
-        return minDiff;
-    }
-
     public int minimizeTheDifference(int[][] mat, int target) {
         int n = mat.length;
-        Integer[][] dp = new Integer[n][5001];
-        return helper(mat, target, 0, 0, dp);
+        int m = mat[0].length;
+        int maxSum = 70 * 70;
+        int[][] dp = new int[n + 1][maxSum + 1];
+
+        for (int sum = 0; sum <= maxSum; sum++) {
+            dp[n][sum] = Math.abs(sum - target);
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int sum = 0; sum <= maxSum; sum++) {
+                int minDiff = Integer.MAX_VALUE;
+
+                for (int j = 0; j < m; j++) {
+                    if (sum + mat[i][j] <= maxSum) {
+                        minDiff = Math.min(
+                            minDiff,
+                            dp[i + 1][sum + mat[i][j]]
+                        );
+                    }
+                }
+
+                dp[i][sum] = minDiff;
+            }
+        }
+        return dp[0][0];
     }
 }
+
