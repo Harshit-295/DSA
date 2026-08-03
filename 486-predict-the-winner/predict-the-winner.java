77aee623fcc0ab2474977d2f3e36d9837be6c117
Harshit Bhardwaj <bhardwajharsh1984@gmail.com>
2026-08-03 21:25:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (6.97%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,14 +1,25 @@
 class Solution {
+
+    Integer[][] dp;
+
     int helper(int[] nums, int start, int end) {
         if (start == end) {
             return nums[start];
         }
+        if (dp[start][end] != null) {
+            return dp[start][end];
+        }
         int takeLeft = nums[start] - helper(nums, start + 1, end);
         int takeRight = nums[end] - helper(nums, start, end - 1);
-        return Math.max(takeLeft, takeRight);
+        dp[start][end] = Math.max(takeLeft, takeRight);
+        return dp[start][end];
     }
 
     public boolean predictTheWinner(int[] nums) {
-        return helper(nums, 0, nums.length - 1) >= 0;
+
+        int n = nums.length;
+        dp = new Integer[n][n];
+
+        return helper(nums, 0, n - 1) >= 0;
     }
 }
