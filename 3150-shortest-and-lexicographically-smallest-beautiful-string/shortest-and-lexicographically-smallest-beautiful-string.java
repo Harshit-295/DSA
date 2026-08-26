5d03d778d3c15687274bebed2c27c40109d88821
Harshit Bhardwaj <bhardwajharsh1984@gmail.com>
2026-08-26 19:02:03 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (40.94%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -2,30 +2,25 @@ class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
         int l = 0;
         int c = 0;
-        int n = s.length();
-        ArrayList<String> list = new ArrayList<>();
-        for (int r = 0; r < n; r++) {
+        String ans = "";
+        for (int r = 0; r < s.length(); r++) {
             if (s.charAt(r) == '1') {
                 c++;
             }
             while (c == k) {
-                list.add(s.substring(l, r + 1));
+                String curr = s.substring(l, r + 1);
+                if (ans.equals("") ||
+                    curr.length() < ans.length() ||
+                    (curr.length() == ans.length() && curr.compareTo(ans) < 0)) {
+                    ans = curr;
+                }
                 if (s.charAt(l) == '1') {
                     c--;
                 }
-
                 l++;
             }
         }
-        if (list.isEmpty()) {
-            return "";
-        }
-        Collections.sort(list, (a, b) -> {
-             if (a.length() != b.length()) {
-                return a.length() - b.length();
-            }
-            return a.compareTo(b);
-        });
-          return list.get(0);
+
+        return ans;
     }
 }
